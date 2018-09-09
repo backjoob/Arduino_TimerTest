abb05a8e7f7a470fa0fbe67ee524e887ea051f4d
unlisted <unlisted@unlis.ted>
2018-09-08 21:23:45 -0400

    counts down from 15min

--- TimerTest.ino
+++ TimerTest.ino
@@ -1,11 +1,27 @@
 #include <Adafruit_LEDBackpack.h>
 
-void setup() {
-  // put your setup code here, to run once:
+Adafruit_7segment matrix = Adafruit_7segment();
 
+unsigned long start_time = 0;
+unsigned long duration = (15UL * 60UL * 1000UL); // Minutes to ms
+
+void setup() {
+  Serial.begin(115200);
+  matrix.begin(0x70);
+  start_time = millis();
+  Serial.print("Boot time: ");
+  Serial.println(start_time);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  unsigned long remaining = duration - millis() + start_time;
+  unsigned long seconds = remaining / 1000;
+  unsigned long minutes = seconds / 60;
+  seconds = seconds % 60;
+  unsigned long display_val = (minutes * 100) + seconds;
 
+  matrix.print(display_val, DEC);
+  matrix.drawColon(true);
+  matrix.writeDisplay();
+  delay(1000);
 }
